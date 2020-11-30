e52e13189c4453390457ff91dba0be03886423e8
Ajitem Sahasrabuddhe <ajitem.s@outlook.com>
2020-11-30 21:35:35 +0530

    update example

--- example/main.go
+++ example/main.go
@@ -8,11 +8,13 @@ import (
 func main() {
 	r := role.NewRole(role.Admin)
 
+	// Prints: Admin
 	fmt.Println(r)
 
 	r.AddRole(role.Analytic)
-
+	// Prints: Analytic, Admin
 	fmt.Println(r)
 
+	// Prints: true true false
 	fmt.Println(r.IsRole(role.Admin), r.IsRole(role.Analytic), r.IsRole(role.Curator))
 }
